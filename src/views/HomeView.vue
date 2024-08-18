5b457747dfc105c097d2c949368ac0c847d9bc9d
pigeoniscoding <mariem112002@orange.fr>
2024-08-18 20:55:50 +0200

    tamagochi wrapper

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <PetVue />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import PetVue from "@/components/PetVue.vue";
 
 export default {
   name: "HomeView",
   components: {
-    HelloWorld,
+    PetVue,
   },
 };
 </script>
