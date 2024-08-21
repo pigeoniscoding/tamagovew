10419446c1ce5b02bd63c7bd1858e87da1be71b2
pigeoniscoding <mariem112002@orange.fr>
2024-08-21 19:23:55 +0200

    ability to create a new pet

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,6 @@
-<template>
-  <div class="home">
-    <PetVue />
-  </div>
-</template>
-
 <script>
 // @ is an alias to /src
+import { reactive, toRefs } from "vue";
 import PetVue from "@/components/PetVue.vue";
 
 export default {
@@ -13,5 +8,34 @@ export default {
   components: {
     PetVue,
   },
+  setup() {
+    const state = reactive({
+      petname: "",
+      userinput: "",
+      showCreatePetForm: true,
+    });
+
+    const createpet = () => {
+      state.petname = state.userinput;
+      state.showCreatePetForm = false;
+    };
+
+    return {
+      ...toRefs(state),
+      createpet,
+    };
+  },
 };
 </script>
+
+<template>
+  <div class="home">
+    <h1>{{ petname }}</h1>
+    <PetVue />
+    <form v-if="showCreatePetForm" @submit.prevent>
+      <label for="pet-name">pet name</label>
+      <input type="text" id="pet-name" v-model="userinput" />
+      <button @click="createpet">New Pet</button>
+    </form>
+  </div>
+</template>
